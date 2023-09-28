9dced83498634d274c26f9c7985c31c1a58bcc36
elen281185 <elen281185@gmail.com>
2023-09-28 23:55:31 +0300

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,6 @@ import throttle from 'lodash.throttle';
 const LS_KEY = 'feedback-form-state';
 
 form = document.querySelector('.feedback-form');
-
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
 
